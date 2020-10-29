4457f75d16fb09c8cacf00d015e6046e8dfb1cff
ChubbyFace <522009006@qq.com>
2020-10-29 15:46:18 +0800

    update EmailConfigServiceImpl

--- src/main/java/com/schedule/getmail/bean/request/AddEmailConfigRequest.java
+++ src/main/java/com/schedule/getmail/bean/request/AddEmailConfigRequest.java
@@ -3,10 +3,15 @@ package com.schedule.getmail.bean.request;
 
 import lombok.Data;
 
+import java.sql.Timestamp;
+
 @Data
 public class AddEmailConfigRequest {
 
     String username;
     String email;
     String password;
+    Timestamp startTime;
+    String keyWord;
+    String keyEmail;
 }
--- src/main/java/com/schedule/getmail/service/impl/EmailConfigServiceImpl.java
+++ src/main/java/com/schedule/getmail/service/impl/EmailConfigServiceImpl.java
@@ -31,20 +31,26 @@ public class EmailConfigServiceImpl extends ServiceImpl<EmailConfigMapper, Email
     public boolean saveOrUpdate(AddEmailConfigRequest request) {
         int flag  = 0;
         //根据userName判断是否已经绑定
-        String email = request.getEmail();
-        EmailConfig emailConfig = new EmailConfig();
+        String username = request.getUsername();
         //获取当前时间
         Timestamp time = new Timestamp(System.currentTimeMillis());
         EmailConfig emailConfig1 = emailConfigMapper.selectOne(new QueryWrapper<EmailConfig>().lambda()
-                .eq(!StringUtils.isEmpty(email), EmailConfig::getEmail,email)
+                .eq(!StringUtils.isEmpty(username), EmailConfig::getUsername,username)
         );
         if(CheckUtil.isEmpty(emailConfig1)){
+            EmailConfig emailConfig = new EmailConfig();
             //添加用户名
             emailConfig.setUsername(request.getUsername());
             //添加邮箱
             emailConfig.setEmail(request.getEmail());
             //添加密码
             emailConfig.setPassword(request.getPassword());
+            //添加邮箱同步开始时间
+            emailConfig.setStartTime(request.getStartTime());
+            //添加过滤关键词
+            emailConfig.setKeyWord(request.getKeyWord());
+            //添加过滤邮箱
+            emailConfig.setKeyEmail(request.getKeyEmail());
             //创建时间
             emailConfig.setCreatetime(time);
             //更新时间
@@ -56,16 +62,22 @@ public class EmailConfigServiceImpl extends ServiceImpl<EmailConfigMapper, Email
            flag =  emailConfigMapper.insert(emailConfig);
         }else {
             //添加邮箱
-            emailConfig.setEmail(request.getEmail());
+            emailConfig1.setEmail(request.getEmail());
             //添加密码
-            emailConfig.setPassword(request.getPassword());
+            emailConfig1.setPassword(request.getPassword());
+            //添加邮箱同步开始时间
+            emailConfig1.setStartTime(request.getStartTime());
+            //添加过滤关键词
+            emailConfig1.setKeyWord(request.getKeyWord());
+            //添加过滤邮箱
+            emailConfig1.setKeyEmail(request.getKeyEmail());
             //更新时间
-            emailConfig.setUpdatetime(time);
+            emailConfig1.setUpdatetime(time);
             //密码加密方式
-            emailConfig.setEncrypt("1");
+            emailConfig1.setEncrypt("1");
             //状态
-            emailConfig.setFlag("1");
-            flag =  emailConfigMapper.updateById(emailConfig);
+            emailConfig1.setFlag("1");
+            flag =  emailConfigMapper.updateById(emailConfig1);
         }
         return flag > 0;
     }
